f5202de2cef823ba6c4cb3db2bd8b65fb11e4404
bezawit_tsige345 <bezawittsige2445@GMAIL.COM>
2026-01-31 23:02:10 +0300

    Login

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-         FirebaseService.initialize();
-        SwingUtilities.invokeLater(() -> new AppFrame());
-    }
-}
